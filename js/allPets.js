ba2c37448a865400b310ae367f4b8be84169e0a7
Habibur Rahman <habibur88454@gmail.com>
2024-10-05 21:13:14 +0600

    added value in the card

--- js/allPets.js
+++ js/allPets.js
@@ -8,33 +8,33 @@ const displayAllPets = (pets) => {
     const allPetsContainer = document.getElementById('all-pets-container');
     pets.forEach(pet => {
         console.log(pet);
-        const {breed, date_of_birth,gender,price,pet_name} =pet
+        const { image, breed, date_of_birth, gender, price, pet_name } = pet
         const div = document.createElement('div');
         div.innerHTML = `
         <div class="border p-5 rounded-lg">
                             <figure>
                                 <img class="rounded-lg"
-                                    src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
+                                    src="${image}"
                                     alt="">
                             </figure>
                             <div class="space-y-2">
-                                <h2 class="card-title mt-4">Pets name!</h2>
+                                <h2 class="card-title mt-4">${pet_name}</h2>
                                 <div class="text-gray-600 space-y-2 border-b">
                                     <div class="flex items-center gap-1">
                                         <i class="fa-solid fa-wind h-5 w-5"></i>
-                                        <p>Breed: </p>
+                                        <p>Breed: ${breed ? breed : 'Not Found'}</p>
                                     </div>
                                     <div class="flex items-center gap-1">
                                         <i class="fa-regular fa-calendar h-5 w-5"></i>
-                                        <p>Birth: </p>
+                                        <p>Birth: ${date_of_birth ? date_of_birth : 'Not Found'}</p>
                                     </div>
                                     <div class="flex items-center gap-1">
                                         <i class="fa-solid fa-venus h-5 w-5"></i>
-                                        <p>Gender: </p>
+                                        <p>Gender: ${gender ? gender : 'Not Found'}</p>
                                     </div>
                                     <div class="flex items-center gap-1 border-b pb-4">
                                         <i class="fa-solid fa-dollar-sign h-5 w-5"></i>
-                                        <p>Price: </p>
+                                        <p>Price: ${price}$</p>
                                     </div>
                                 </div>
 
